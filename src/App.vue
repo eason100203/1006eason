7d9fee74b3675edb37662ae10d72318153d478b3
eason100203 <137794204+eason100203@users.noreply.github.com>
2023-10-06 09:15:00 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+    import { RouterView } from 'vue-router';
+    import { ref, computed } from 'vue';
+    import { ElButton, ElInput, ElDatePicker } from 'element-plus';
+  
+ const selectedColor = ref('#00FF00'); 
+ const selectedColorbor = ref(getRandomColor());
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+  
+  function updateBorderColor() {
+  selectedColorbor.value = getRandomColor();
+}
+</script>
+
+<template>
+    <div class="color-box" :style="{ backgroundColor: selectedColor,border:'6px solid'+selectedColorbor}">
+      <H1>顏色選擇器</H1>
+      <div>
+  <input type="color" v-model="selectedColor" @input="updateBorderColor" />
+      </div>
+      <p>選擇的顏色: {{ selectedColor }}</p>
+    </div>
+</template>
+<style>
+.color-box {
+  width: 100%;
+  height: 1000px;
+  display: block;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-size: 18px;
+}
+</style>
